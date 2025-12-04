fc0e6d1a19bc9d88cfba0c1976b3b504f3ef2ef1
Karthik Naresh Panyam <85242011+KarthikPanyam@users.noreply.github.com>
2025-12-04 18:36:44 -0500

    Add files via upload

---
+++ saberv2.ino
@@ -0,0 +1,202 @@
+#include<Adafruit_NeoPixel.h>
+
+#define ledPin 6
+#define numLeds 144
+
+
+#define buttonPin 2
+#define button2Pin 3
+
+
+int lastbuttonState = HIGH;
+unsigned int lastDebounce = 0;
+unsigned int debounceDelay = 150;
+
+
+int lastbutton2State = HIGH;
+unsigned int lastDebounce2 = 0;
+unsigned int debounceDelay2 = 150;
+
+
+
+
+
+
+
+static int currentPixel = 0;
+static unsigned long pixelDelay = 0;
+
+
+Adafruit_NeoPixel pixels(numLeds, ledPin, NEO_GRB + NEO_KHZ800);
+
+
+
+
+uint32_t red = pixels.Color(100, 0,0);
+uint32_t green = pixels.Color(0,100,0);
+uint32_t blue = pixels.Color(0,0,100);
+uint32_t cyan = pixels.Color(0,20,100);
+uint32_t bladeColor;
+
+static int colorCount = 0;
+int colorLimit = 4;
+
+
+
+
+
+
+
+enum State {
+			OFF,
+			IGNITION,
+			RETRACTION};
+			
+State currentState, nextState;
+
+
+
+unsigned long stateStartTime = 0;
+unsigned long bladeDelay = 2;
+
+
+
+void setup()
+{
+	pinMode(buttonPin, INPUT_PULLUP);
+	pinMode(button2Pin, INPUT_PULLUP);
+	
+	pixels.begin();
+	pixels.clear();
+	pixels.show();
+	delay(10);
+	bladeColor = cyan;
+	currentState = OFF;
+	
+}
+
+
+void loop()
+{
+	int currentButton = digitalRead(buttonPin);
+	int currentButton2 = digitalRead(button2Pin);
+	
+	if (currentButton2 != lastbutton2State)
+	{
+		if(currentButton2 == LOW && lastbutton2State == HIGH)
+		{
+			if(millis() - lastDebounce2 > debounceDelay2)
+				{
+					
+					colorChange();
+					
+					colorCount++;				//Write the color code here
+					
+					if(colorCount >= colorLimit)
+						colorCount = 0;
+					
+					lastDebounce2 = millis();
+					
+				}
+		}
+	}
+	
+	
+	
+	if (currentButton != lastbuttonState)
+	{
+		if(currentButton == LOW && lastbuttonState == HIGH)
+		{
+			if(millis() - lastDebounce > debounceDelay)
+				{
+					currentState = nextState;
+					lastDebounce = millis();
+				}
+		}
+	}
+	
+	lastbuttonState = currentButton;
+	lastbutton2State = currentButton2;
+	
+	switch(currentState)
+	{
+		case OFF: 
+			nextState = IGNITION;
+			break;
+		
+		case IGNITION:
+			nextState = RETRACTION;
+			break;
+			
+		case RETRACTION:
+			nextState = OFF;
+			break;
+	}
+	
+	
+	switch(currentState)
+	{
+		case OFF:
+			currentPixel = 0;
+			pixels.clear();
+			pixels.show();
+			break;
+			
+			
+		case IGNITION:
+				if(currentPixel < numLeds)
+				{
+					if(millis() - pixelDelay >= bladeDelay)
+					{
+						pixels.setPixelColor(currentPixel, bladeColor);
+						pixels.show();
+						currentPixel++;
+						pixelDelay = millis();
+					}
+				}
+				break;
+			
+		case RETRACTION:
+			if(currentPixel >0)
+				{
+					if(millis() - pixelDelay >= bladeDelay)
+					{
+						pixels.setPixelColor(currentPixel -1, 0,0,0);
+						pixels.show();
+						currentPixel--;
+						pixelDelay = millis();
+						
+					}
+				}	
+			else
+				currentState = OFF;
+			break;
+			
+	}
+	
+}
+
+
+void colorChange()
+{
+	switch(colorCount)
+	{
+		
+		case 0: bladeColor = red;
+				break;
+				
+		case 1: bladeColor = blue;
+				break;
+		
+		case 2: bladeColor = green;
+				break;
+				
+		case 3: bladeColor = cyan;
+				break;
+				
+		default: bladeColor = cyan;
+				break;
+				
+	}
+	
+}
