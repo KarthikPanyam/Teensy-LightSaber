4f9ee346afdb6790d22244e5cac9dfd3aa6ab302
Karthik Naresh Panyam <85242011+KarthikPanyam@users.noreply.github.com>
2025-12-05 17:30:17 -0500

    Update saberv2.ino

--- saberv2.ino
+++ saberv2.ino
@@ -44,9 +44,9 @@ static int currentPixel = 0;
 uint32_t bladeColor;
 
 enum State {
-OFF,
-IGNITION,
-RETRACTION};
+  OFF,
+  IGNITION,
+  RETRACTION};
 
 State currentState, nextState;
 
@@ -59,81 +59,83 @@ unsigned long bladeDelay = 2;
 
 void setup()
 {
-pinMode(buttonPin, INPUT_PULLUP);
-pinMode(button2Pin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(button2Pin, INPUT_PULLUP);
 
-pixels.begin();
-pixels.clear();
-pixels.show();
-delay(10);
-bladeColor = cyan;
-currentState = OFF;
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+  delay(10);
+  bladeColor = cyan;
+  currentState = OFF;
 
 }
 
 
 void loop()
 {
-int currentButton = digitalRead(buttonPin);
-int currentButton2 = digitalRead(button2Pin);
+  int currentButton = digitalRead(buttonPin);
+  int currentButton2 = digitalRead(button2Pin);
 
 // ====== Debounce Logic for Colour button - Button 2 ======
 
-if (currentButton2 != lastbutton2State)
-{
-if(currentButton2 == LOW && lastbutton2State == HIGH)
-{
-if(millis() - lastDebounce2 > debounceDelay2)
-{
-if (currentPixel == numLeds){
-colorChange();
-pixels.show();
-colorCount++;
-}
+  if (currentButton2 != lastbutton2State)
+  {
+    if(currentButton2 == LOW && lastbutton2State == HIGH)
+    {
+      if(millis() - lastDebounce2 > debounceDelay2)
+      {
+        if (currentPixel == numLeds)
+        {
+          colorCount++;
+          colorChange();
+          pixels.show();
+          
+        }
 
 
-if(colorCount >= colorLimit)
-colorCount = 0;
+        if(colorCount >= colorLimit)
+          colorCount = 0;
 
-lastDebounce2 = millis();
+        lastDebounce2 = millis();
 
-}
-}
-}
+      }
+    }
+  }
 // ====== END of Debounce Logic for Colour button - Button 2 ======
 
-// ====== Debounce Logic for State change button - Button 2 ======
+// ====== Debounce Logic for State change button - Button 1 ======
 if (currentButton != lastbuttonState)
 {
-if(currentButton == LOW && lastbuttonState == HIGH)
-{
-if(millis() - lastDebounce > debounceDelay)
-{
-currentState = nextState;
-lastDebounce = millis();
-}
-}
+  if(currentButton == LOW && lastbuttonState == HIGH)
+  {
+    if(millis() - lastDebounce > debounceDelay)
+    {
+      currentState = nextState;
+      lastDebounce = millis();
+    }
+  }
 }
 
 lastbuttonState = currentButton;
 lastbutton2State = currentButton2;
-// ====== END Debounce Logic for State change button - Button 2 ======
+// ====== END Debounce Logic for State change button - Button 1 ======
 
 
 // ====== Next State Logic ======
 switch(currentState)
 {
-case OFF:
-nextState = IGNITION;
-break;
+  case OFF:
+            nextState = IGNITION;
+            break;
 
-case IGNITION:
-nextState = RETRACTION;
-break;
+  case IGNITION:
+            nextState = RETRACTION;
+            break;
 
-case RETRACTION:
-nextState = OFF;
-break;
+  case RETRACTION:
+            nextState = OFF;
+            break;
 }
 
 // ====== END of Next State Logic ======
@@ -142,43 +144,42 @@ break;
 // ====== Output Logic ======
 switch(currentState)
 {
-case OFF:
-currentPixel = 0;
-pixels.clear();
-pixels.show();
-break;
+  case OFF:
+        currentPixel = 0;
+        pixels.clear();
+        pixels.show();
+        break;
 
 
-case IGNITION:
-if(currentPixel < numLeds)
-{
-if(millis() - pixelDelay >= bladeDelay)
-{
-pixels.setPixelColor(currentPixel, bladeColor);
-pixels.show();
-currentPixel++;
-pixelDelay = millis();
-}
-}
-break;
+    case IGNITION:
+        if(currentPixel < numLeds)
+        {
+          if(millis() - pixelDelay >= bladeDelay)
+            {
+              pixels.setPixelColor(currentPixel, bladeColor);
+              pixels.show();
+              currentPixel++;
+              pixelDelay = millis();
+            }
+          }
+          break;
 
-case RETRACTION:
-if(currentPixel >0)
-{
-if(millis() - pixelDelay >= bladeDelay)
-{
-pixels.setPixelColor(currentPixel -1, 0,0,0);
-pixels.show();
-currentPixel--;
-pixelDelay = millis();
+  case RETRACTION:
+         if(currentPixel >0)
+          {
+            if(millis() - pixelDelay >= bladeDelay)
+              {
+                pixels.setPixelColor(currentPixel -1, 0,0,0);
+                pixels.show();
+                currentPixel--;
+                pixelDelay = millis();
+              }
+            }
+          else
+            currentState = OFF;
+          break;
 
-}
-}
-else
-currentState = OFF;
-break;
-
-}
+  }
 
 // ====== END of Output Logic ======
 
@@ -187,25 +188,25 @@ break;
 
 void colorChange()  // Colour change function
 {
-switch(colorCount)
-{
+  switch(colorCount)
+  {
 
-case 0: bladeColor = red;
-break;
+    case 0: bladeColor = red;
+            break;
 
-case 1: bladeColor = blue;
-break;
+    case 1: bladeColor = blue;
+            break;
 
-case 2: bladeColor = green;
-break;
+    case 2: bladeColor = green;
+            break;
 
-case 3: bladeColor = cyan;
-break;
+    case 3: bladeColor = cyan;
+            break;
 
-default: bladeColor = cyan;
-break;
+  default: bladeColor = cyan;
+            break;
 
-}
-
+  }
 
 } // END OF Colour Change Function
+
