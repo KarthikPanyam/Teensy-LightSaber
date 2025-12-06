6fc27af98a919533048cd7f2516f1c8c2684c776
Karthik Naresh Panyam <85242011+KarthikPanyam@users.noreply.github.com>
2025-12-05 23:19:51 -0500

    Update saberv2.ino

--- saberv2.ino
+++ saberv2.ino
@@ -19,7 +19,7 @@ unsigned int debounceDelay2 = 150;
 
 
 // === Delay per pixel for animation ===
-static unsigned long pixelDelay = 0;
+static unsigned long pixelDelay = 5;
 
 
 Adafruit_NeoPixel pixels(numLeds, ledPin, NEO_GRB + NEO_KHZ800); // Creating "pixels" object
@@ -30,9 +30,10 @@ Adafruit_NeoPixel pixels(numLeds, ledPin, NEO_GRB + NEO_KHZ800); // Creating "pi
 uint32_t red = pixels.Color(100, 0,0);
 uint32_t green = pixels.Color(0,100,0);
 uint32_t blue = pixels.Color(0,0,100);
+uint32_t purple = pixels.Color(50,0,50);
 uint32_t cyan = pixels.Color(0,20,100);
 
-int colorLimit = 4; // number of colours
+int colorLimit = 5; // number of colours
 // === END of COLOUR CONTROL ===
 
 
@@ -59,6 +60,8 @@ unsigned long bladeDelay = 2;
 
 void setup()
 {
+
+	Serial.begin(9600);
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
 
@@ -66,6 +69,27 @@ void setup()
   pixels.clear();
   pixels.show();
   delay(10);
+
+  pixels.setPixelColor(1, cyan);
+  pixels.show();
+  delay(300);
+
+  pixels.setPixelColor(1, red);
+  pixels.show();
+  delay(300);
+
+  pixels.setPixelColor(1, blue);
+  pixels.show();
+  delay(300);
+
+  pixels.setPixelColor(1, green);
+  pixels.show();
+  delay(300);
+
+  pixels.setPixelColor(1, purple);
+  pixels.show();
+  delay(1000);
+
   bladeColor = cyan;
   currentState = OFF;
 
@@ -74,6 +98,7 @@ void setup()
 
 void loop()
 {
+	
   int currentButton = digitalRead(buttonPin);
   int currentButton2 = digitalRead(button2Pin);
 
@@ -85,17 +110,20 @@ void loop()
     {
       if(millis() - lastDebounce2 > debounceDelay2)
       {
-        if (currentPixel == numLeds)
+        if (currentPixel == numLeds || currentPixel == 0)
         {
+					
           colorCount++;
+          Serial.println(colorCount);
+					if(colorCount >= colorLimit)
+         		 colorCount = 0;
           colorChange();
           pixels.show();
           
         }
 
 
-        if(colorCount >= colorLimit)
-          colorCount = 0;
+
 
         lastDebounce2 = millis();
 
@@ -111,6 +139,7 @@ if (currentButton != lastbuttonState)
   {
     if(millis() - lastDebounce > debounceDelay)
     {
+			Serial.println("state change");
       currentState = nextState;
       lastDebounce = millis();
     }
@@ -191,16 +220,18 @@ void colorChange()  // Colour change function
   switch(colorCount)
   {
 
-    case 0: bladeColor = red;
+    case 0: bladeColor = green;
             break;
 
-    case 1: bladeColor = blue;
+    case 1: bladeColor = red;
             break;
 
-    case 2: bladeColor = green;
+    case 2: bladeColor = purple;
             break;
 
     case 3: bladeColor = cyan;
+            break;
+    case 4: bladeColor = blue;
             break;
 
   default: bladeColor = cyan;
@@ -209,4 +240,3 @@ void colorChange()  // Colour change function
   }
 
 } // END OF Colour Change Function
-
